4dc3c1615300697bd2d3a0a9aff8697e54ed0ccd
Aaron Decker <decker49@gmail.com>
2020-05-15 08:05:55 -0500

    Bump to 1.0.0. The API has been tested and is being used in production, so this is ready to be out of alpha.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.stronganchortech/couchdb-auth-for-ring "0.1.0-SNAPSHOT"
+(defproject com.stronganchortech/couchdb-auth-for-ring "1.0.0"
   :description "Ring middleware that lets you use CouchDB's security model for you Ring app."
   :url "https://github.com/deckeraa/couchdb-auth-for-ring/"
   :license {:name "EPL-2.0 OR GPL-3.0-or-later WITH Classpath-exception-2.0"
