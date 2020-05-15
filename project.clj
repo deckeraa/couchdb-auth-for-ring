4f3df98bd88d0c447a1b5e74b0baf551cd026991
Aaron Decker <decker49@gmail.com>
2020-05-15 07:42:27 -0500

    Update :url in project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.stronganchortech/couchdb-auth-for-ring "0.1.0-SNAPSHOT"
   :description "Ring middleware that lets you use CouchDB's security model for you Ring app."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/deckeraa/couchdb-auth-for-ring/"
   :license {:name "EPL-2.0 OR GPL-3.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
