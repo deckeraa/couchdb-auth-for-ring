429ea1d3be33fd52bc3bbbe1eca885b9fe00d539
Aaron Decker <decker49@gmail.com>
2020-06-06 05:59:54 -0500

    Bump to 1.0.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.stronganchortech/couchdb-auth-for-ring "1.0.1"
+(defproject com.stronganchortech/couchdb-auth-for-ring "1.0.2"
   :description "Ring middleware that lets you use CouchDB's security model for your Ring app."
   :url "https://github.com/deckeraa/couchdb-auth-for-ring/"
   :license {:name "EPL-2.0 OR GPL-3.0-or-later WITH Classpath-exception-2.0"
